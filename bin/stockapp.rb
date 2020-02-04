fa2460f443dc20f2e71bd666ece820121238b59f
thomas dubiel <github email address>
2020-02-04 17:48:50 -0600

    start invest method in controller class

--- bin/stockapp.rb
+++ bin/stockapp.rb
@@ -5,31 +5,5 @@ require_relative '../config/environment'
 require_relative '../config/iex-client.rb'
 
 Controller.new.invest
-user = User.new("Tom") 
-stock = Stock.new("Tesla", "TSLA") 
-puts user.stocks 
-user.add_stock(stock) 
-puts "Your stocks are #{user.stocks}"
-puts stock.name 
-puts Stock.all_stocks 
 
 
-stock = IEX::Api::Client.new(
-	publishable_token: 'Tpk_dd88c906f3ae4ac492644c2d0d82281d',
-	endpoint: 'https://sandbox.iexapis.com/v1'
-	)
-company = stock.company('MSFT')
-
-puts company.ceo # 'Satya Nadella'
-puts company.company_name
-key_stats = stock.key_stats("MSFT") 
-
-puts "the number of employees at #{company.company_name} is #{key_stats.employees}"
-puts "the 52 week dollar change is #{key_stats.week_52_change_dollar}"
-puts "the 52 week high at #{company.company_name} is #{key_stats.week_52_high}"
-
-puts "#{company.company_name}: #{stock.price("MSFT")}"
-puts "#{stock.company("IBM").company_name}: #{stock.price("IBM")}"
-puts stock.price("F")
-puts stock.price("ENPH") 
-puts stock.price("TSLA")
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,62 @@
 class Controller 
-	def invest 
-		puts "Let's make some money!"
+
+	def initialize
+		
 	end
+	def invest 
+
+		stock = IEX::Api::Client.new(
+			publishable_token: 'Tpk_dd88c906f3ae4ac492644c2d0d82281d',
+			endpoint: 'https://sandbox.iexapis.com/v1'
+			) 
+
+		puts "Let's make some money!" 
+		puts "If you would like to search for stocks, type in the ticker symbol."
+		puts "Please enter the ticker symbol of the stock you want" 
+		response = nil 
+
+		while response != "exit"
+			ticker = gets.chomp 
+			puts ticker
+			price = stock.price(ticker.upcase) 
+			puts price 
+			puts "would you like to exit?" 
+			response = gets.chomp
+		end
+	end 
+
+
+		
+
+
+=begin
+		user = User.new("Tom") 
+		stock = Stock.new("Tesla", "TSLA") 
+		puts user.stocks 
+		user.add_stock(stock) 
+		puts "Your stocks are #{user.stocks}"
+		puts stock.name 
+		puts Stock.all_stocks 
+
+
+		stock = IEX::Api::Client.new(
+			publishable_token: 'Tpk_dd88c906f3ae4ac492644c2d0d82281d',
+			endpoint: 'https://sandbox.iexapis.com/v1'
+			)
+		company = stock.company('MSFT')
+
+		puts company.ceo # 'Satya Nadella'
+		puts company.company_name
+		key_stats = stock.key_stats("MSFT") 
+
+		puts "the number of employees at #{company.company_name} is #{key_stats.employees}"
+		puts "the 52 week dollar change is #{key_stats.week_52_change_dollar}"
+		puts "the 52 week high at #{company.company_name} is #{key_stats.week_52_high}"
+
+		puts "#{company.company_name}: #{stock.price("MSFT")}"
+		puts "#{stock.company("IBM").company_name}: #{stock.price("IBM")}"
+		puts stock.price("F")
+		puts stock.price("ENPH") 
+		puts stock.price("TSLA") 
+=end
 end
