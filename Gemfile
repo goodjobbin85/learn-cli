3be53989f702762e6d687b0607b5e9f12cb2ec86
thomas dubiel <github email address>
2020-02-13 22:38:13 -0600

    add pry restclient httparty dependencies to gemfile

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source "https://rubygems.org"
 
 # gem "rails" 
 gem 'iex-ruby-client'
+gem 'httparty' 
+gem 'rest-client' 
+gem 'json' 
+gem 'pry'
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,14 @@
 #environment file for requirements and load files
 
 #require_relative '../config/iex-client.rb' 
-
+require 'httparty' 
+require 'rest-client' 
+require 'json' 
+require 'pry'
 #require_all 'lib'
 require_relative '../lib/stock' 
 require_relative '../lib/user' 
 require_relative '../lib/portfolio' 
 require_relative '../lib/bank'
-require_relative '../lib/controller'
+require_relative '../lib/controller' 
+
